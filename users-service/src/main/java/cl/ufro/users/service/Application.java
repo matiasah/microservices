d71cfe69ef6fbcc680dd51004d1c1a2048540924
Matías Hermosilla <matiasahermosilla@gmail.com>
2019-08-25 23:17:10 -0400

    Configurando servidor de recursos

--- users-service/src/main/java/cl/ufro/users/service/Application.java
+++ users-service/src/main/java/cl/ufro/users/service/Application.java
@@ -8,6 +8,7 @@ package cl.ufro.users.service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 /**
  *
@@ -15,6 +16,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
  */
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableResourceServer
 public class Application {
     
     public static void main(String [] args) {
